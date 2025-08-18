e1a71d3f2e0d7bb374633c7dc57b1e55341e27b0
GitHub Action <action@github.com>
2025-08-18 07:44:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/andret2344/achievementsforintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/andret2344/achievementsforintellij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.andret2344.achievementsforintellij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.andret2344.achievementsforintellij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
--- src/main/kotlin/com/github/andret2344/achievementsforintellij/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/andret2344/achievementsforintellij/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.andret2344.achievementsforintellij.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
